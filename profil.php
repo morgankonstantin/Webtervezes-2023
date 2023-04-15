b9e4de7ca2535811b677f8b7d1ebb16b6d1ef02f
Konstantin Morgan <konstantin.morgan@gmail.com>
2023-04-15 19:58:14 +0200

    complete profil.php add style for profil.php

--- profil.php
+++ profil.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["user"])) {
+    header("Location: login.php");
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="hu">
 
@@ -37,18 +47,116 @@
         </nav>
     </header>
     <main>
-        <h1>Bejelentkezés</h1>
-        <section class="urlap">
-            <h3>Üdvözöljük</h3>
-            <h4>Írja be az e-mail címét és jelszavát:</h4>
-            <form action="belepes.php" method="POST" enctype="multipart/form-data">
-                <div class="belepes">
-                    <input id="email" name="email" type="email" placeholder="Email" maxlength="320" required /><br />
-                    <input id="jelszo" name="jelszo" type="password" placeholder="Jelszó" maxlength="64" required /><br />
-                    <input type="submit" name="belepes" value="Belépés" /> <br /><br />
-                </div>
+        <h1>Profilom</h1>
+        <table border="1" style="display:flex;justify-content:center;align-items:center" class="profil-form">
+            <form action="profil.php" class="profil-table">
+                <tr>
+                    <th>Adatok</th>
+                    <th>Publikus</th>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="nev">Név</label><br>
+                        <input type="text" id="nev" class="nev" name="nev" value="<?php echo htmlspecialchars($_SESSION["user"]["nev"]); ?>" required>
+                    </td>
+                    <td>
+                        <input type="checkbox" name="pub_nev" id="pub_nev" <?= $_SESSION["user"]["pub_nev"] ?>>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="jelszo">Jelszó</label><br>
+                        <input type="password" id="jelszo" class="jelszo" name="jelszo" value="<?php echo htmlspecialchars($_SESSION["user"]["jelszo"]); ?>">
+                    </td>
+                    <td>
+                        <input type="hidden" name="jelszo">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="jelszoism">Jelszó megerősítése</label><br>
+                        <input type="password" id="jelszo" class="jelszo" value="<?php echo htmlspecialchars($_SESSION["user"]["jelszo"]); ?>">
+                    </td>
+                    <td>
+                        <input type="hidden" name="jelszoism">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="email">Email</label><br>
+                        <input type="email" id="email" class="email" name="email" value="<?php echo htmlspecialchars($_SESSION["user"]["email"]); ?>">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="pub_email" id="pub_email" <?= $_SESSION["user"]["pub_email"] ?>>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="tel">Tel.:</label><br>
+                        <input type="tel" id="tel" class="tel" name="tel" value="<?php echo htmlspecialchars($_SESSION["user"]["tel"]); ?>">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="pub_tel" id="pub_tel" <?= $_SESSION["user"]["pub_tel"] ?>>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="szul-datum">Születési dátum</label><br>
+                        <input type="date" name="szul-datum" id="szul-datum" class="szul-datum" value="<?php echo htmlspecialchars($_SESSION["user"]["szul-datum"]); ?>">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="pub_szul-datum" id="pub_szul-datum" <?= $_SESSION["user"]["pub_szul-datum"] ?>>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="irszam">Irányítószám</label><br>
+                        <input type="text" name="irszam" id="irszam" value="<?php echo htmlspecialchars($_SESSION["user"]["irszam"]); ?>">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="pub_irszam" id="pub_irszam" <?= $_SESSION["user"]["pub_irszam"] ?>>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="varos">Város</label><br>
+                        <input type="text" name="varos" id="varos" value="<?php echo htmlspecialchars($_SESSION["user"]["varos"]); ?>">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="pub_varos" id="pub_varos" <?= $_SESSION["user"]["pub_varos"] ?>>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="utca">Utca, házszám</label><br>
+                        <input type="text" name="utca" id="utca" value="<?php echo htmlspecialchars($_SESSION["user"]["utca"]); ?>">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="pub_utca" id="pub_utca" <?= $_SESSION["user"]["pub_utca"] ?>>
+                    </td>
+                </tr>
             </form>
-        </section>
+        </table>
+        <div class="kedvenc-bolygo">
+            <fieldset id="kedvenc-bolygo">
+                <legend>Kedvenc bolygó:</legend>
+                <label for="kedvenc-merkur">Merkúr: </label> <input id="kedvenc-merkur" name="kedvenc" type="radio" value="merkur" />
+                <label for="kedvenc-venusz">Vénusz: </label> <input id="kedvenc-venusz" name="kedvenc" type="radio" value="venusz" /><br />
+                <label for="kedvenc-fold">Föld: </label> <input id="kedvenc-fold" name="kedvenc" type="radio" value="fold" checked />
+                <label for="kedvenc-mars">Mars: </label> <input id="kedvenc-mars" name="kedvenc" type="radio" value="mars" /><br />
+                <label for="kedvenc-jupiter">Jupiter: </label> <input id="kedvenc-jupiter" name="kedvenc" type="radio" value="jupiter" />
+                <label for="kedvenc-szaturnusz">Szaturnusz: </label> <input id="kedvenc-szaturnusz" name="kedvenc" type="radio" value="szaturnusz" /><br />
+                <label for="kedvenc-uranusz">Uránusz: </label> <input id="kedvenc-uranusz" name="kedvenc" type="radio" value="uranusz" />
+                <label for="kedvenc-neptunusz">Neptunusz: </label> <input id="kedvenc-neptunusz" name="kedvenc" type="radio" value="neptunusz" />
+                <div class="pub-kedvenc">
+                    <input type="checkbox" name="pub_kedvenc" id="pub_kedvenc" <?= $_SESSION["user"]["pub_kedvenc"] ?>>
+                    <label for="pub_kedvenc" id="pub-kedvenc-label">Publikus</label>
+                </div>
+            </fieldset>
+        </div>
+        <div class="mentes">
+            <input type="submit" value="Mentés">
+        </div>
     </main>
     <footer>
         <p>Készítők: Konstantin Morgan</p>
